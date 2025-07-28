2eeca6ad453badb4c0508bc43022ae9271e1d242
Ludovico Furlanetto <ludovico.furlanetto@gmail.com>
2025-07-28 10:53:35 +0200

    maybe now the config is complete

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 14
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
+}
+config.color_scheme = "Catppuccin Mocha"
+
+config.default_cwd = "/Users/notludovico/Code"
+
+config.window_background_opacity = 0.6
+config.macos_window_background_blur = 10
+
+local act = wezterm.action
+config.keys = {
+	{
+		key = "k",
+		mods = "CMD",
+		action = act.Multiple({
+			-- wipe the scrollback *and* the viewport
+			act.ClearScrollback("ScrollbackAndViewport"),
+			-- send Ctrl‑L to your shell to redraw prompt
+			act.SendKey({ key = "L", mods = "CTRL" }),
+		}),
+	},
+}
+
+return config
